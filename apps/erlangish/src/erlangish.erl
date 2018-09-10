def14d4471b5dd1a98ef33cb08d2902563ba01a4
vereis <me@cbailey.co.uk>
2018-09-10 22:55:16 +0100

    Added getopt dependency as well as modified escript entrypoint to parse command args. Based accepted flags on flags accepted by Jarlang

--- apps/erlangish/src/erlangish.erl
+++ apps/erlangish/src/erlangish.erl
@@ -4,5 +4,38 @@
     main/1
 ]).
 
+-define(OPTS, [
+    {help,      $h,  "help",      undefined,         "Show help and usage message"},
+    {version,   $v,  "version",   undefined,         "Show application version"},
+    {processes, $p, "processes",  {integer, -1},     "Number of concurrent processes to use"},
+    {export,    $e,  "export",    {string, "js"},    "Compiler output format"},
+    {output,    $o,  "output",    {string, "./"},    "Output directory"}
+]).
+
+-define(VSN, "0.0.1").
+
+-define(TERNARY(C, T, F), case C of true -> T; false -> F end).
+
+%% EScript entrypoint
+-spec main(list(any())) -> ok.
 main(Args) ->
-    Args.
+    case getopt:parse(?OPTS, Args) of
+        {ok, {ParsedArgs, ArgTokens}} ->
+            ok = ?TERNARY(lists:member(help,    ParsedArgs), help(),    ok),
+            ok = ?TERNARY(lists:member(version, ParsedArgs), version(), ok),
+            io:format("Args: ~p :: ~p", [ParsedArgs, ArgTokens]);
+        _ ->
+            help()
+    end,
+    ok.
+
+%% Internal functions
+-spec help() -> ok.
+help() ->
+    getopt:usage(?OPTS, ?MODULE_STRING, "[file ...]"),
+    ok.
+
+-spec version() -> ok.
+version() ->
+    io:format("~s version: ~s~n", [?MODULE_STRING, ?VSN]),
+    ok.
